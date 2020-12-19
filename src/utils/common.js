0f3f144e329f1e2f584ffd43ce8ee013bf777d92
Artem Cherenkov <art.cherenkov@gmail.com>
2020-12-19 13:32:07 +0700

    Добавляет утилитарные функции и перечисления статусов, приоритетов и просрочки

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,6 +1,8 @@
 import moment from 'moment';
 
-export const getRandomDate = (start, end) => moment(start + Math.random() * (end.diff(start)));
+export const getRandomDate = (start, end) => (
+  moment(start + Math.random() * (end.diff(start)))
+);
 
 export const getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -8,4 +10,26 @@ export const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-export const getRandomArrayItem = (arr) => arr[getRandomInt(0, arr.length - 1)];
+export const getRandomArrayItem = (arr) => (
+  arr[getRandomInt(0, arr.length - 1)]
+);
+
+export const getRandomObjectItem = (obj) => (
+  getRandomArrayItem(Object.keys(obj))
+);
+
+
+export const reverseObj = (obj) => (
+  Object
+    .entries(obj)
+    .reduce((acc, entry) => {
+      const [key, value] = entry;
+      acc = {...acc, [value]: key}
+      return acc;
+    }, {})
+);
+
+export const getKeyByValue = (obj, value) => (
+  reverseObj(obj)[value].toLowerCase()
+);
+
