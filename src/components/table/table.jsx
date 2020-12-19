4d7c1ccdcf0bb84867d44a8af0b5311872cb09dc
Artem Cherenkov <art.cherenkov@gmail.com>
2020-12-19 13:44:46 +0700

    Избавляется от захардкоженных строк таблицы

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -26,90 +26,10 @@ export default function Table({requests}) {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>205</td>
-              <td>25.09.2020 17:09</td>
-              <td>Программный продукт 'ABC_SNB_Compiler'</td>
-              <td>Надеин Алексей/ООО НПП АВС-Н</td>
-              <td>Доработка</td>
-              <td>Трекер АВС</td>
-              <td>Разработчики</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__status table__status--opened">Открыто</td>
-              <td>29.09.2020 18:00</td>
-              <td>–</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__priority table__priority--normal">Нормальный</td>
-              <td className="table__expired table__expired--no"><span>Не просрочен</span></td>
-            </tr>
-            <tr>
-              <td>205</td>
-              <td>25.09.2020 17:09</td>
-              <td>Программный продукт 'ABC_SNB_Compiler'</td>
-              <td>Надеин Алексей/ООО НПП АВС-Н</td>
-              <td>Доработка</td>
-              <td>Трекер АВС</td>
-              <td>Разработчики</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__status table__status--opened">Ожидается в релизе</td>
-              <td>29.09.2020 18:00</td>
-              <td>–</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__priority table__priority--high">Высокий</td>
-              <td className="table__expired table__expired--yes"><span>Просрочен</span></td>
-            </tr>
-            <tr>
-              <td>205</td>
-              <td>25.09.2020 17:09</td>
-              <td>Программный продукт 'ABC_SNB_Compiler'</td>
-              <td>Надеин Алексей/ООО НПП АВС-Н</td>
-              <td>Доработка</td>
-              <td>Трекер АВС</td>
-              <td>Разработчики</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__status table__status--closed">Закрыто</td>
-              <td>29.09.2020 18:00</td>
-              <td>–</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__priority table__priority--low">Низкий</td>
-              <td className="table__expired table__expired--no"><span>Не просрочен</span></td>
-            </tr>
-            <tr>
-              <td>205</td>
-              <td>25.09.2020 17:09</td>
-              <td>Программный продукт 'ABC_SNB_Compiler'</td>
-              <td>Надеин Алексей/ООО НПП АВС-Н</td>
-              <td>Доработка</td>
-              <td>Трекер АВС</td>
-              <td>Разработчики</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__status table__status--declined">Отклонено</td>
-              <td>29.09.2020 18:00</td>
-              <td>–</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__priority table__priority--normal">Нормальный</td>
-              <td className="table__expired table__expired--no"><span>Не просрочен</span></td>
-            </tr>
-            <tr>
-              <td>205</td>
-              <td>25.09.2020 17:09</td>
-              <td className="table__description">Программный продукт 'ABC_SNB_Compiler'</td>
-              <td>Надеин Алексей/ООО НПП АВС-Н</td>
-              <td>Доработка</td>
-              <td>Трекер АВС</td>
-              <td>Разработчики</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__status table__status--opened">Открыто</td>
-              <td>29.09.2020 18:00</td>
-              <td>–</td>
-              <td>Новиков Дмитрий</td>
-              <td className="table__priority table__priority--normal">Нормальный</td>
-              <td className="table__expired table__expired--no"><span>Не просрочен</span></td>
-            </tr>
             {requests.map((request, i) => <Row key={`request-${i}`} request={request} />)}
           </tbody>
         </table>
       </div>
     </section>
   )
-}
+};
