ece0a25138bfd5b0caae7294d041ceceeb93e64b
Artem Cherenkov <art.cherenkov@gmail.com>
2020-12-23 01:54:48 +0700

    Поднимает сборку, меняет URLы на внешние

--- src/utils/fetch-api.js
+++ src/utils/fetch-api.js
@@ -2,14 +2,14 @@ import {loadIssues} from '../store/action';
 
 // временные функции
 export const fetchIssues = async (store) => {
-  const res = await fetch(`http://localhost:5000/issues`, {
+  const res = await fetch(`https://express-api-newbie.herokuapp.com/issues`, {
     withCredentials: true
   });
   store.dispatch(loadIssues(await res.json()))
 }
 
 export const postIssue = async (issue) => {
-  let res = await fetch('http://localhost:5000/issues', {
+  let res = await fetch('https://express-api-newbie.herokuapp.com/issues', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
